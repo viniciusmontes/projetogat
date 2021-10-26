ab51597de74572e8f04a7dcf8e3ff085de6e5c27
Vinicius Montes <viniciusmontes@outlook.com.br>
2021-10-25 23:46:38 -0300

    Projeto Criado

---
+++ src/main/java/com/entrevista/projetogat/ProjetogatApplication.java
@@ -0,0 +1,13 @@
+package com.entrevista.projetogat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetogatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetogatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrevista/projetogat/ProjetogatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrevista.projetogat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetogatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
